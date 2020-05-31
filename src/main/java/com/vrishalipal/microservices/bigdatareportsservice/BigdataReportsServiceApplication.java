6944a7a579de25a8b842780c35d9c422f1edadd0
vjpal3 <vrishalipal@yahoo.com>
2020-05-30 22:23:53 -0400

    Initial commit

---
+++ src/main/java/com/vrishalipal/microservices/bigdatareportsservice/BigdataReportsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vrishalipal.microservices.bigdatareportsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataReportsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigdataReportsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrishalipal/microservices/bigdatareportsservice/BigdataReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrishalipal.microservices.bigdatareportsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
